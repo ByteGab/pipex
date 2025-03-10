beaf02fd53f7eb4d3db4688bf0cb06516c8be9ca
Bytegab <gabrielfreiresimon@gmail.com>
2025-03-10 03:16:27 +0100

    create pipex

---
+++ pipex.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 02:45:02 by gafreire          #+#    #+#             */
+/*   Updated: 2025/03/10 03:16:07 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+void	child(void)
+{
+}
+
+void	parent(void)
+{
+}
+
+int main(int argc,char *argv[])
+{
+    if (argc != 5)
+    {
+        printf("No es valido\n");
+    }
+    else
+    {
+        // if exist infile and outfile
+        // create pipe
+        // child proccess
+        // parent proccess
+        // wait to all procces
+        // execute execve
+    }
+}
