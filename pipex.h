6b25d99836471e6b16fa44059c540adff898c5c4
Bytegab <gabrielfreiresimon@gmail.com>
2025-03-22 01:17:44 +0100

    changes pipex.h

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 02:39:31 by gafreire          #+#    #+#             */
-/*   Updated: 2025/03/15 23:36:46 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/03/21 23:41:12 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PIPEX_H
 
 #include <unistd.h>
+#include "libft/libft.h"
 #include <stdio.h>
  #include <fcntl.h>
 #endif
